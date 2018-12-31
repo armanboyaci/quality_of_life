ef4607038641cd7c941e743e97ee0c6d094495c0
Arman Boyacı <arman@Armans-MacBook-Pro.local>
2018-12-31 13:16:09 +0300

    first commit

---
+++ code/.ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,9 @@
+class model(object):
+    def load_data():
+        pass
+    
+    def train():
+        pass
+
+    def predict():
+        pass
---
+++ docs/problem_description.md
@@ -0,0 +1,7 @@
+# How to predict the quality of life of a country?
+
+WHAT
+
+WHY
+
+HOW
